a1446b50f237fee4ede8d8790d47a7d5a81d2f82
akreyes21 <164187372+akreyes21@users.noreply.github.com>
2024-04-11 02:41:59 +0000

    done adding output

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
+public class Main{
+public static void main(String[] args) {
+    def largest_number(num1, num2, num3):
+    if num1 == num2 == num3:
+        return "All numbers are equal"
+    else:
+        return max(num1, num2, num3)
 
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+print("The largest number is:", largest_number(num1, num2, num3))
+
+}
